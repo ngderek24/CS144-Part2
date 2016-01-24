b58f51c30773985c4bcc4ba988e2fbda4c05d660
jasony0206 <jasony0206@gmail.com>
2016-01-23 16:08:47 -0800

    Query #3 works

--- queries.sql
+++ queries.sql
@@ -4,11 +4,15 @@ select count(*) from (select UserID from Sellers union select UserID from Bidder
 -- 2 off by 1 --
 select count(*) from Items where Location = "New York";
 
+-- 3 done --
+select count(*) from (select * from ItemCategory group by ItemID having count(*) = 4) as T;
+
+-- 4 --
+
+
 -- 5 done --
 select count(*) from Sellers where SellerRating > 1000;
 
 -- 6 off -- 
 select count(*) from Sellers s, Bidders b where s.UserID = b.UserID;
 
--- 3 --
-select count(*) from ItemCategory group by ItemID having count(Category) = 4;
