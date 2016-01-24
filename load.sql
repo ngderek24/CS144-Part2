71501a087a81e7231b0fecf89f3c3c6b0f148226
jasony0206 <jasony0206@gmail.com>
2016-01-24 15:38:21 -0800

    Change load.sql to not use our own path

--- load.sql
+++ load.sql
@@ -1,17 +1,17 @@
-LOAD DATA LOCAL INFILE '~/shared/project2/CS144-Part2/items-uniq.csv' 
+LOAD DATA LOCAL INFILE 'items-uniq.csv' 
 INTO TABLE Items FIELDS TERMINATED BY '\t' OPTIONALLY ENCLOSED BY '"';
 
-LOAD DATA LOCAL INFILE '~/shared/project2/CS144-Part2/location-info-uniq.csv' 
+LOAD DATA LOCAL INFILE 'location-info-uniq.csv' 
 INTO TABLE LocationInfo FIELDS TERMINATED BY '\t' OPTIONALLY ENCLOSED BY '"';
 
-LOAD DATA LOCAL INFILE '~/shared/project2/CS144-Part2/item-category-uniq.csv' 
+LOAD DATA LOCAL INFILE 'item-category-uniq.csv' 
 INTO TABLE ItemCategory FIELDS TERMINATED BY '\t' OPTIONALLY ENCLOSED BY '"';
 
-LOAD DATA LOCAL INFILE '~/shared/project2/CS144-Part2/sellers-uniq.csv' 
+LOAD DATA LOCAL INFILE 'sellers-uniq.csv' 
 INTO TABLE Sellers FIELDS TERMINATED BY '\t' OPTIONALLY ENCLOSED BY '"';
 
-LOAD DATA LOCAL INFILE '~/shared/project2/CS144-Part2/bidders-uniq.csv' 
+LOAD DATA LOCAL INFILE 'bidders-uniq.csv' 
 INTO TABLE Bidders FIELDS TERMINATED BY '\t' OPTIONALLY ENCLOSED BY '"';
 
-LOAD DATA LOCAL INFILE '~/shared/project2/CS144-Part2/bids-uniq.csv' 
+LOAD DATA LOCAL INFILE 'bids-uniq.csv' 
 INTO TABLE Bids FIELDS TERMINATED BY '\t' OPTIONALLY ENCLOSED BY '"';
