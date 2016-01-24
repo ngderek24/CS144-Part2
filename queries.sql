7d8a5f99d1c76ae7d7b2ed5ca577117c935bc7a3
jasony0206 <jasony0206@gmail.com>
2016-01-23 22:23:11 -0800

    Query#4 works

--- queries.sql
+++ queries.sql
@@ -1,18 +1,31 @@
 -- 1 done --
-select count(*) from (select UserID from Sellers union select UserID from Bidders) as Users;
+select count(*) from 
+(select UserID from Sellers union select UserID from Bidders) as Users;
 
 -- 2 off by 1 --
-select count(*) from Items where Location = "New York";
+select count(*) from Items 
+where Location = "New York";
 
 -- 3 done --
-select count(*) from (select * from ItemCategory group by ItemID having count(*) = 4) as T;
+select count(*) from 
+(select * from ItemCategory 
+	group by ItemID 
+	having count(*) = 4) as T;
 
 -- 4 --
-
+select ItemID from Items 
+where Ends > '2001-12-20 00:00:01'
+and Number_of_Bids > 0
+and Currently >= all 
+(select Currently from Items 
+	where Ends > '2001-12-20 00:00:01' 
+	and Number_of_Bids > 0);
 
 -- 5 done --
-select count(*) from Sellers where SellerRating > 1000;
+select count(*) from Sellers 
+where SellerRating > 1000;
 
 -- 6 off -- 
-select count(*) from Sellers s, Bidders b where s.UserID = b.UserID;
+select count(*) from Sellers s, Bidders b 
+where s.UserID = b.UserID;
 
