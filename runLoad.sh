bd0300d3a3fd9dc3886f6c482c893e4acf73c325
tincan24 <tincan24@gmail.com>
2016-01-28 13:10:07 -0800

    Changed database from TEST to CS144

--- runLoad.sh
+++ runLoad.sh
@@ -2,10 +2,10 @@
 
 # Run the drop.sql batch file to drop existing tables
 # Inside the drop.sql, you sould check whether the table exists. Drop them ONLY if they exists.
-mysql TEST < drop.sql
+mysql CS144 < drop.sql
 
 # Run the create.sql batch file to create the database and tables
-mysql TEST < create.sql
+mysql CS144 < create.sql
 
 # Compile and run the parser to generate the appropriate load files
 ant
@@ -20,7 +20,7 @@ sort bidders.csv | uniq > bidders-uniq.csv
 sort bids.csv | uniq > bids-uniq.csv
 
 # Run the load.sql batch file to load the data
-mysql TEST < load.sql
+mysql CS144 < load.sql
 
 # Remove all temporary files
 rm *.csv
